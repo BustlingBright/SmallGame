cb43af036c89da5882eb3882e66621e9706b7c6f
username@email.com <435629456@qq.com>
2019-04-23 20:48:16 +0800

    no message

--- Assets/Scripts/old/LevelManager.cs
+++ Assets/Scripts/old/LevelManager.cs
@@ -8,29 +8,50 @@ public class LevelManager : MonoBehaviour {
     public GameObject ememyNomal;
     public GameObject enemyBig;
 
-	public float rateTime = 2;
-	float myTime;
+	public float rateTime1 = 0;
+    public float rateTime2 = 0;
+    public float rateTime3 = 0;
+    float myTime;
 
 	void Awake () {
 		Im = this;
 	}
 	
-    void CreateEnemy(float time,GameObject monster)
+    void CreateEnemy1(float time,GameObject monster)
     {
-        time += Time.deltaTime;
-        if (time > rateTime)
+        rateTime1 += Time.deltaTime;
+        if (rateTime1 > time)
         {
-
             Vector2 r = Random.insideUnitCircle.normalized * 400;
             Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
-            time -= rateTime;
+            rateTime1 =0;
+        }
+    }
+    void CreateEnemy2(float time, GameObject monster)
+    {
+        rateTime2 += Time.deltaTime;
+        if (rateTime2 > time)
+        {
+            Vector2 r = Random.insideUnitCircle.normalized * 400;
+            Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
+            rateTime2 = 0;
+        }
+    }
+    void CreateEnemy3(float time, GameObject monster)
+    {
+        rateTime3 += Time.deltaTime;
+        if (rateTime3 > time)
+        {
+            Vector2 r = Random.insideUnitCircle.normalized * 400;
+            Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
+            rateTime3 = 0;
         }
     }
 
-	// Update is called once per frame
-	void Update () {
-        CreateEnemy(2, enemy);
-        CreateEnemy(4, ememyNomal);
-        CreateEnemy(6, enemyBig);
+    // Update is called once per frame
+    void Update () {
+        CreateEnemy1(2, enemy);
+        CreateEnemy2(4, ememyNomal);
+        CreateEnemy3(6, enemyBig);
     }
 }
