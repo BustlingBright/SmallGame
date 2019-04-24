b35ee81c6ffb6495c32010ecb3e6920475d7edfd
username@email.com <435629456@qq.com>
2019-04-24 17:57:35 +0800

    no message

---
+++ Assets/Scripts/old/ConfigManger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+struct Config
+{
+    public int id;
+    public string mapName;
+    public Vector3 playerPosition;
+    public Vector3 playerSkillPosition;
+    public Vector3 monsterPosition;
+    public Vector3 cameraFirstPosition;
+    public Vector3 cameraFirstRotation;
+    public Vector3 cameraLastPosition;
+    public Vector3 cameraLastRotation;
+    public Vector3 mapCameraPosition;
+}
+
+
+class ConfigManger
+{
+    private static ConfigManger instance;
+    public static ConfigManger Instance
+    {
+        get
+        {
+            if(instance==null)
+            {
+                instance = new ConfigManger();
+            }
+            return instance;
+        }
+    }
+
+    private string path = "levelConfig.txt";
+    Dictionary<int, Config> config = new Dictionary<int, Config>();
+
+    private ConfigManger() {
+        Init();
+    }
+
+    void Init()
+    {
+        if (File.Exists(path))
+        {
+            StreamReader sr = new StreamReader(path);
+            string str;
+            while ((str= sr.ReadLine())!=null)
+            {
+                string[] tempConfig = str.Split(new char[] { ' ' });
+                Config _config;
+                _config.id = int.Parse(tempConfig[0]);
+                _config.mapName = tempConfig[1];
+                _config.playerPosition = new Vector3(float.Parse(tempConfig[2]), float.Parse(tempConfig[3]), float.Parse(tempConfig[4]));
+                _config.playerSkillPosition= new Vector3(float.Parse(tempConfig[5]), float.Parse(tempConfig[6]), float.Parse(tempConfig[7]));
+                _config.monsterPosition= new Vector3(float.Parse(tempConfig[8]), float.Parse(tempConfig[9]), float.Parse(tempConfig[10]));
+                //////////未做
+                _config.cameraFirstPosition= new Vector3(float.Parse(tempConfig[11]), float.Parse(tempConfig[12]), float.Parse(tempConfig[13]));
+                _config.cameraFirstRotation= new Vector3(float.Parse(tempConfig[14]), float.Parse(tempConfig[15]), float.Parse(tempConfig[16]));
+                _config.cameraLastPosition= new Vector3(float.Parse(tempConfig[17]), float.Parse(tempConfig[18]), float.Parse(tempConfig[19])); 
+                _config.cameraLastRotation= new Vector3(float.Parse(tempConfig[20]), float.Parse(tempConfig[21]), float.Parse(tempConfig[22]));
+                _config.mapCameraPosition= new Vector3(float.Parse(tempConfig[23]), float.Parse(tempConfig[24]), float.Parse(tempConfig[25]));
+                config.Add(_config.id, _config);
+            }
+        }
+    }
+
+    public Config GetConfig(int id)
+    {
+        if(config.ContainsKey(id))
+        {
+            return config[id];
+        }
+        return new Config();
+    }
+
+}
+
+
--- Assets/Scripts/old/LevelManager.cs
+++ Assets/Scripts/old/LevelManager.cs
@@ -1,21 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour {
-	static public LevelManager Im;
+
 	public Transform player;
-	public GameObject enemy;
-    public GameObject ememyNomal;
-    public GameObject enemyBig;
+    public Transform skill2;
+	private GameObject enemy0;
+    private GameObject enemy1;
+    private GameObject enemy2;
+    private new Transform camera;
+    private Transform mapCamera; 
 
 	public float rateTime1 = 0;
     public float rateTime2 = 0;
     public float rateTime3 = 0;
-    float myTime;
 
-	void Awake () {
-		Im = this;
+    private int levelId;
+    private Config config;
+    private List<GameObject> monsters = new List<GameObject>();
+    private bool isCreate = false;
+	void Awake ()
+    {
+        levelId = 1001;
+        config = ConfigManger.Instance.GetConfig(levelId);
+        camera = Camera.main.transform;
+        mapCamera = GameObject.Find("MapCamera").transform;
+        Init();
 	}
+
+    void Init()
+    {
+        isCreate = false;
+        for (int i = 0; i < monsters.Count; i++)
+        {
+            if(monsters[i]!=null)
+            {
+                Destroy(monsters[i]);
+            }
+        }
+        ///玩家位置
+        player.position = config.playerPosition;
+        player.rotation = Quaternion.identity;
+        ///玩家武器位置
+        skill2.position = config.playerSkillPosition;
+        skill2.rotation = Quaternion.identity;
+        ///怪物类型
+        enemy0 = Resources.Load("Role/" + config.mapName + "/0") as GameObject;
+        enemy1 = Resources.Load("Role/" + config.mapName + "/1") as GameObject;
+        enemy2 = Resources.Load("Role/" + config.mapName + "/2") as GameObject;
+        mapCamera.position = config.mapCameraPosition;
+        isCreate = true;
+    }
+
+    public void LevelUp()
+    {
+        isCreate = false;
+        if(levelId<1005)
+        {
+            levelId++;
+            Init();
+        }
+
+    }
+
 	
     void CreateEnemy1(float time,GameObject monster)
     {
@@ -23,8 +71,9 @@ public class LevelManager : MonoBehaviour {
         if (rateTime1 > time)
         {
             Vector2 r = Random.insideUnitCircle.normalized * 400;
-            Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
+            GameObject g= Instantiate(monster, new Vector3(r.x+config.monsterPosition.x, config.monsterPosition.y, r.y+config.monsterPosition.z), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
             rateTime1 =0;
+            monsters.Add(g);
         }
     }
     void CreateEnemy2(float time, GameObject monster)
@@ -33,8 +82,9 @@ public class LevelManager : MonoBehaviour {
         if (rateTime2 > time)
         {
             Vector2 r = Random.insideUnitCircle.normalized * 400;
-            Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
+            GameObject g= Instantiate(monster, new Vector3(r.x + config.monsterPosition.x, config.monsterPosition.y, r.y + config.monsterPosition.z), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0))); rateTime2 = 0;
             rateTime2 = 0;
+            monsters.Add(g);
         }
     }
     void CreateEnemy3(float time, GameObject monster)
@@ -43,15 +93,19 @@ public class LevelManager : MonoBehaviour {
         if (rateTime3 > time)
         {
             Vector2 r = Random.insideUnitCircle.normalized * 400;
-            Instantiate(monster, new Vector3(r.x, 20, r.y), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
+            GameObject g= Instantiate(monster, new Vector3(r.x + config.monsterPosition.x, config.monsterPosition.y, r.y + config.monsterPosition.z), Quaternion.Euler(new Vector3(0, Random.Range(0.0f, 360.0f), 0)));
             rateTime3 = 0;
+            monsters.Add(g);
         }
     }
 
     // Update is called once per frame
     void Update () {
-        CreateEnemy1(2, enemy);
-        CreateEnemy2(4, ememyNomal);
-        CreateEnemy3(6, enemyBig);
+        if(isCreate)
+        {
+            CreateEnemy1(2, enemy0);
+            CreateEnemy2(4, enemy1);
+            CreateEnemy3(6, enemy2);
+        }
     }
 }
